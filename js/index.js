98339b1f6717485a6a54fc86cacb4226922e9f14
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2022-10-30 18:49:34 +0530

    Added js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+console.log("{Working}");
+ScrollReveal({ reset: true });
+ScrollReveal().reveal(".headline");
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    delay: 900,
+    interval: 500
+};
+var Up = {
+    distance: '150%',
+    origin: 'bottom',
+    delay: 900,
+    interval: 500
+};
+var mintUp= {
+    distance: '150%',
+    origin: 'bottom',
+    delay: 900,
+    interval: 500
+};
+ScrollReveal().reveal(".onego", slideUp);
+ScrollReveal().reveal(".oneg", Up);
+ScrollReveal().reveal(".one", mintUp);
